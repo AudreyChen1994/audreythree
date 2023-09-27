d0bed1b1848929231b6f5dce8d4f7681d89852fb
Audrey1994 <1399059621@qq.com>
2023-09-27 12:55:32 +0800

    模块

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-import pageChangeListener from './src/pageChangeListener.js';
-import pageScrollListener  from './src/pageScrollListener.js';
-export default{
+// import pageChangeListener from './src/pageChangeListener.js';
+// import pageScrollListener  from './src/pageScrollListener.js';
+// export default{
+//     pageChangeListener,
+//     pageScrollListener
+// }
+const {pageChangeListener,pageScrollListener} = require('./src/useListener');
+// pageChangeListener();
+module.exports={
     pageChangeListener,
     pageScrollListener
 }
